943d636465d9da5f04cabe84d59c84ffe973e7da
Nafissa <loadjianaffissatou@gmail.com>
2025-03-20 10:32:01 +0100

    Pendu le jeu

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Pendu;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{	NavigationPage.SetHasBackButton(this, false);
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		await Navigation.PushAsync(new JeuPage());
+	}
+}
+
